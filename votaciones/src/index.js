e3e240c762f1eddfa550ab7a7ba3895cf447b392
daniElPapu <daniel.bedergal@tecsup.edu.pe>
2021-04-14 09:37:07 -0500

    Se agrego un condicional render para el componente Statitics

--- votaciones/src/index.js
+++ votaciones/src/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -15,7 +14,7 @@ const App = () => {
         <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
         <button onClick={()=>setBad(bad+1)}>bad</button>
       </div>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      {(good>0||neutral>0||bad>0)?<Statistics good={good} neutral={neutral} bad={bad}/>:<p>No Feedback given</p>}
     </div>
   )
 }
